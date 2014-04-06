a590ff062500290ed1d203c4b4893d3c7cbc0cef
Eric Zeiberg <masterejzz4820@gmail.com>
2014-04-06 09:54:32 -0400

    Fix thread async

--- src/me/masterejay/testapp/Thread.java
+++ src/me/masterejay/testapp/Thread.java
@@ -1,6 +1,7 @@
 package me.masterejay.testapp;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -67,49 +68,78 @@ public class Thread extends Activity implements CustomWebView.OnBottomReachedLis
 		}
 	}
 
+	protected class ThreadParse extends AsyncTask<String, String, String>{
+		public void parsePage(String link){
+			try {
+				doc = Jsoup.connect(link).userAgent("Mozilla").get();
+			} catch (IOException e) {
+				e.printStackTrace();
+				Toast.makeText(Thread.this, "Something went wrong! Are you sure you're online?", Toast.LENGTH_LONG).show();
+				return;
+			}
+			final Element e = doc.getElementsByClass("span9").get(0);
+			Elements buttons = doc.getElementsByClass("btn-group");
+
+			Element pages = null;
+			try {
+				pages = buttons.addClass("pull-left").get(0);
+			}
+			catch (IndexOutOfBoundsException ex){
+				Thread.this.runOnUiThread(new Runnable() {
+
+					public void run() {
+						currentPage = 1;
+						totalPages = 1;
+						Toast.makeText(Thread.this, "You are on " + currentPage + " of " + totalPages, Toast.LENGTH_LONG).show();
+						webview.loadDataWithBaseURL("http://netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css", e.html(), "text/html", "utf-8", null);
+						return;
+
+					}
+				});
+
+			}
+			Elements buttonEach = null;
+			if(pages != null){
+				buttonEach = pages.getElementsByClass("btn");
+			}
+
+			Log.d("STUFF", pages.text());
+
+			Log.d("STUFF",String.valueOf(buttonEach.size()));
+			for (int i = 0; i < buttonEach.size(); i++){
+				Element e1 = buttonEach.get(i);
+				if (e1.hasClass("disabled") && !e1.text().equals("…")){
+					currentPage = Integer.parseInt(e1.text());
+				}
+				else if (e1.text().contains("Last")){
+					Log.d("STUFF A", e1.text());
+					totalPages = Integer.parseInt(e1.attr("href").split("=")[1]);
+				}
+			}
+			Thread.this.runOnUiThread(new Runnable() {
+
+				public void run() {
+					Toast.makeText(Thread.this, "You are on " + currentPage + " of " + totalPages, Toast.LENGTH_LONG).show();
+					if (webview != null){
+						webview.loadDataWithBaseURL("http://netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css", e.html(),"text/html","utf-8",null);
+					}
+
+				}
+			});
+		}
+
+		@Override
+		protected String doInBackground(String... params){
+			parsePage(params[0]);
+			return null;
+		}
+	}
+
 
 	public void initWebView(String link){
+		ThreadParse parse = new ThreadParse();
+		parse.execute(link);
 
-		try {
-			doc = Jsoup.connect(link).userAgent("Mozilla").get();
-		} catch (IOException e) {
-			e.printStackTrace();
-			Toast.makeText(Thread.this, "Something went wrong! Are you sure you're online?", Toast.LENGTH_LONG).show();
-			return;
-		}
-		Element e = doc.getElementsByClass("span9").get(0);
-		Elements buttons = doc.getElementsByClass("btn-group");
-
-		Element pages;
-		try {
-			 pages = buttons.addClass("pull-left").get(0);
-		}
-		catch (IndexOutOfBoundsException ex){
-			currentPage = 1;
-			totalPages = 1;
-			Toast.makeText(Thread.this, "You are on " + currentPage + " of " + totalPages, Toast.LENGTH_LONG).show();
-			webview.loadDataWithBaseURL("http://netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css", e.html(), "text/html", "utf-8", null);
-			return;
-		}
-		Elements buttonEach = pages.getElementsByClass("btn");
-
-		Log.d("STUFF", pages.text());
-
-		Log.d("STUFF",String.valueOf(buttonEach.size()));
-		for (int i = 0; i < buttonEach.size(); i++){
-			Element e1 = buttonEach.get(i);
-			if (e1.hasClass("disabled") && !e1.text().equals("…")){
-				currentPage = Integer.parseInt(e1.text());
-			}
-			else if (e1.text().contains("Last")){
-				Log.d("STUFF A", e1.text());
-				totalPages = Integer.parseInt(e1.attr("href").split("=")[1]);
-			}
-		}
-		Toast.makeText(Thread.this, "You are on " + currentPage + " of " + totalPages, Toast.LENGTH_LONG).show();
-		if (webview != null){
-			webview.loadDataWithBaseURL("http://netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css", e.html(),"text/html","utf-8",null);
-		}
 	}
 
 
